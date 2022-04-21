02f8a2817ac1b18db6b342c22eee57bb11f39a08
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-21 15:53:21 -0300

    Fragment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 
 function Home() {
   return (
+    <div>
    <h1>Lista de Presença</h1>
+   <input type="text" placeholder="Digite o nome..." />
+   <button>Adicionar</button>
+   </div>
   )
 }
 
