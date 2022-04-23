c553d510c5c5d6a1ddaa692b5c74fb54c00d7115
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-23 17:28:09 -0300

    immutability in react

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,16 +5,34 @@ import { Card } from '../../components/Card';
 
  export function Home() {
    const [studentName, setStudentName] = useState('')
+   const [students, setStudents] = useState([])
+
+   function handleAddStudent(){
+
+     const newStudent = {
+       name: studentName,
+       time: new Date().toLocaleTimeString("pt-br", {
+         hour: '2-digit',
+         minute: '2-digit',
+         second: '2-digit'
+       })
+     };
+
+     setStudents(prevState => [...prevState, newStudent])
+   }
+
+   
 
   return (
     <div className="container">
-   <h1>Nome: {studentName}</h1>
+   <h1>Lista de Presença</h1>
    <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)} />
-   <button>Adicionar</button>
+   <button onClick={handleAddStudent}>Adicionar</button>
 
-   <Card name="Bruno" time="10:55:25"/>
-   <Card name="Juliana" time="11:00:10"/>
-   <Card name="Daniel" time="12:30:25"/>
+   {
+     students.map(student =>  <Card name={student.name} time={student.time}/>)
+  }
+   
    </div>
   )
 }
