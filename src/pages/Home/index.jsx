dc1a584565d18ce3200fbb1bcd521673353b64fd
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-23 15:30:10 -0300

    using properties in react

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,9 +9,9 @@ import { Card } from '../../components/Card';
    <input type="text" placeholder="Digite o nome..." />
    <button>Adicionar</button>
 
-   <Card />
-   <Card />
-   <Card />
+   <Card name="Bruno" time="10:55:25"/>
+   <Card name="Juliana" time="11:00:10"/>
+   <Card name="Daniel" time="12:30:25"/>
    </div>
   )
 }
