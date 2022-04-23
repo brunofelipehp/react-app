88749822792b681ee47b8a17575c202b13c87411
brunofelipehp <brunofelipehp8@gmail.com>
2022-04-23 16:37:14 -0300

    Using state in react

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,15 @@
+import React, { useState } from 'react';
 import './styles.css';
 
 import { Card } from '../../components/Card';
 
  export function Home() {
+   const [studentName, setStudentName] = useState('')
+
   return (
     <div className="container">
-   <h1>Lista de Presença</h1>
-   <input type="text" placeholder="Digite o nome..." />
+   <h1>Nome: {studentName}</h1>
+   <input type="text" placeholder="Digite o nome..." onChange={e => setStudentName(e.target.value)} />
    <button>Adicionar</button>
 
    <Card name="Bruno" time="10:55:25"/>
